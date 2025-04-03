82621d6c2d1ba7f4050e317780346815e984e3ad
nicholascang12 <nicholascang12@gmail.com>
2025-04-03 12:41:20 -0400

    Add employee table to users.sql

--- Database/users.sql
+++ Database/users.sql
@@ -24,6 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `emp_id` int(11) NOT NULL,
+  `ssn` varchar(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `m_init` varchar(1) DEFAULT NULL,
+  `lname` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `super_ssn` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`emp_id`, `ssn`, `fname`, `m_init`, `lname`, `role`, `salary`, `super_ssn`) VALUES
+(6, '777-77-7777', 'Ron', 'B', 'Parker', 'Library Owner', 150000, NULL),
+(7, '102-56-3972', 'Nick', 'M', 'Smith', 'Database Admin', 100000, '777-77-7777'),
+(8, '135-79-2468', 'Michelle', 'S', 'Booker', 'Head Librarian', 90000, '777-77-7777'),
+(9, '111-22-3333', 'Robert', 'A', 'Schmidt', 'Librarian', 60000, '135-79-2468'),
+(10, '333-66-9999', 'Rita', NULL, 'Jackson', 'Librarian', 65000, '135-79-2468');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `profiles`
 --
 
